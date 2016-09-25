a0c4a5b52630cb3a3a12a2fa761a523326253846
James Tauber <jtauber@jtauber.com>
2016-09-25 03:00:14 -0400

    added note about why utils are implemented the way they are

--- utils.py
+++ utils.py
@@ -8,6 +8,10 @@ def remove_item(lst, index):
     returns a new list equal to the given list with the item at the given index
     removed
     """
+
+    # In the simple case, making a copy of the list and `del`ing the item is
+    # much faster but I suspect if the items are complex datastructures
+    # themselves, this is more efficient.
     return lst[:index] + lst[index + 1:]
 
 
@@ -16,4 +20,8 @@ def change_item(lst, index, change):
     returns a new list equal to the given list with the item at the given index
     changed by the callable passed in
     """
+
+    # In the simple case, making a copy of the list and changing the item is
+    # much faster but I suspect if the items are complex datastructures
+    # themselves, this is more efficient.
     return lst[:index] + [change(lst[index])] + lst[index + 1:]
